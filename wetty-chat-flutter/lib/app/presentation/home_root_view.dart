7f22242b3021b05ac595536e22932bdd62ec9d53
jue ma <ykhinama@gmail.com>
2026-04-01 14:13:45 -0700

    flutter: restore the general settings page, the message now follow the user selected slider setting. bottom nav bar style change

--- wetty-chat-flutter/lib/app/presentation/home_root_view.dart
+++ wetty-chat-flutter/lib/app/presentation/home_root_view.dart
@@ -1,35 +1,154 @@
 import 'package:flutter/cupertino.dart';
+import 'package:wetty_chat_flutter/shared/presentation/app_divider.dart';
 
 import '../../features/chats/chats.dart';
 import '../../features/settings/settings.dart';
 
-class HomeRootPage extends StatelessWidget {
+class HomeRootPage extends StatefulWidget {
   const HomeRootPage({super.key});
 
   @override
+  State<HomeRootPage> createState() => _HomeRootPageState();
+}
+
+class _HomeRootPageState extends State<HomeRootPage> {
+  int _selectedIndex = 0;
+
+  static const _tabs = [
+    _HomeTabData(icon: CupertinoIcons.chat_bubble_2, label: 'Chats'),
+    _HomeTabData(icon: CupertinoIcons.gear, label: 'Settings'),
+  ];
+
+  late final List<Widget> _tabViews = const [
+    CupertinoTabView(builder: _buildChatsTab),
+    CupertinoTabView(builder: _buildSettingsTab),
+  ];
+
+  static Widget _buildChatsTab(BuildContext context) => const ChatPage();
+
+  static Widget _buildSettingsTab(BuildContext context) => const SettingsPage();
+
+  @override
   Widget build(BuildContext context) {
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.chat_bubble_2),
-            label: 'Chats',
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: IndexedStack(index: _selectedIndex, children: _tabViews),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.gear),
-            label: 'Settings',
+          _BottomNavBar(
+            items: _tabs,
+            selectedIndex: _selectedIndex,
+            onTap: (index) {
+              if (_selectedIndex == index) {
+                return;
+              }
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
           ),
         ],
       ),
-      tabBuilder: (context, index) {
-        switch (index) {
-          case 0:
-            return CupertinoTabView(builder: (_) => const ChatPage());
-          case 1:
-            return CupertinoTabView(builder: (_) => const SettingsPage());
-        }
-        return CupertinoTabView(builder: (_) => const ChatPage());
-      },
     );
   }
+}
+
+class _BottomNavBar extends StatelessWidget {
+  const _BottomNavBar({
+    required this.items,
+    required this.selectedIndex,
+    required this.onTap,
+  });
+
+  final List<_HomeTabData> items;
+  final int selectedIndex;
+  final ValueChanged<int> onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final borderColor = CupertinoColors.separator.resolveFrom(context);
+    final backgroundColor = CupertinoColors.systemBackground.resolveFrom(
+      context,
+    );
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        border: Border(top: BorderSide(color: borderColor, width: 0.5)),
+      ),
+      child: SafeArea(
+        top: false,
+        child: SizedBox(
+          height: 45,
+          child: Row(
+            children: [
+              for (var index = 0; index < items.length; index++)
+                Expanded(
+                  child: _BottomNavItem(
+                    data: items[index],
+                    isSelected: index == selectedIndex,
+                    onTap: () => onTap(index),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _BottomNavItem extends StatelessWidget {
+  const _BottomNavItem({
+    required this.data,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  final _HomeTabData data;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final activeColor = CupertinoColors.activeBlue.resolveFrom(context);
+    final inactiveColor = CupertinoColors.inactiveGray.resolveFrom(context);
+    final color = isSelected ? activeColor : inactiveColor;
+
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: onTap,
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 6),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(data.icon, size: 24, color: color),
+              const SizedBox(height: 3),
+              Text(
+                data.label,
+                style: TextStyle(
+                  color: color,
+                  fontSize: 10,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _HomeTabData {
+  const _HomeTabData({required this.icon, required this.label});
+
+  final IconData icon;
+  final String label;
 }
